efaefc49d124042ec7166f07c435c5c2c86d9204
R24 <42667691+Rezzabodong@users.noreply.github.com>
2018-11-21 10:10:02 +0000

    Create belajar.sh

---
+++ belajar.sh
@@ -0,0 +1,331 @@
+#!/bin/env bash
+clear
+b='\033[34;1m'
+g='\033[32;1m'
+p='\033[35;1m'
+c='\033[36;1m'
+r='\033[31;1m'
+w='\033[37;1m'
+figlet -f Varsity Belajar|lolcat
+figlet -f Varsity Program|lolcat
+echo $r" |-------------------------------------------|"
+echo $g" |                 [Authorr]                 |"
+echo   "[∆]                  [R24]                  [∆]"
+echo   " |             [CreateDate:20:11:18]         |"
+echo $r" |-------------------------------------------|"
+echo ""
+echo ""
+echo $r"==================================="
+echo $g" [1] Belajar Python"
+echo " [2] Belajar PythonV2"
+echo " [3] Belajar Html"
+echo " [4] Belajar Php"
+echo " [5] Belajar Javascript"
+echo " [6] Keluar Aja Ajg!!!!"
+echo $r"===================================="
+read -p "Pilih Angka  : " pil ;
+
+if [ $pil = 1 ] || [ $pil = 1 ]
+then
+clear
+echo "Judul : Belajar python"
+echo "Genre : Pemrograman"
+echo "size  : 1.2 MB"
+echo "Author:"
+sleep 5
+echo $r"Proses."
+sleep 1
+clear
+echo $r"proses.."
+sleep 1
+clear
+echo $r"Proses..."
+sleep 1
+clear
+echo $r"proses...."
+sleep 1
+clear
+echo $r"Proses"
+sleep 1
+clear
+echo $r"proses."
+sleep 1
+clear
+echo $r"Proses.."
+sleep 1
+clear
+echo $r"proses..."
+sleep 1
+clear
+echo $r"Proses...."
+sleep 1
+clear
+echo $r"proses"
+sleep 1
+clear
+echo $r"Proses."
+sleep 1
+clear
+echo $r"proses.."
+sleep 1
+clear
+echo $g"Downloading File"
+sleep 2
+ln -s /sdcard
+cd sdcard
+wget https://ipikli.files.wordpress.com/2011/03/modul-pemrograman-phyton.pdf
+echo ""
+echo "",
+echo $g"Sukses Download!!!"
+echo ""
+echo $r"File Tersimpan Di sdcard!nama modul-pemrograman-python.pdf"
+fi
+
+if [ $pil = 2 ] || [ $pil = 2 ]
+then
+clear
+echo "Judul : Cepat Mahir Python"
+echo "Genre : Pemrograman"
+echo "size  : 1.1 MB"
+sleep 5
+echo $r"Proses."
+sleep 1
+clear
+echo $r"proses.."
+sleep 1
+clear
+echo $r"Proses..."
+sleep 1
+clear
+echo $r"proses...."
+sleep 1
+clear
+echo $r"Proses"
+sleep 1
+clear
+echo $r"proses."
+sleep 1
+clear
+echo $r"Proses.."
+sleep 1
+clear
+echo $r"proses..."
+sleep 1
+clear
+echo $r"Proses...."
+sleep 1
+clear
+echo $r"proses"
+sleep 1
+clear
+echo $r"Proses."
+sleep 1
+clear
+echo $r"proses.."
+sleep 1
+clear
+echo $g"Downloading File"
+sleep 2
+ln -s /sdcard
+cd sdcard
+wget https://topidesta.files.wordpress.com/2015/10/cepat-mahir-python-kuliahkomputer.pdf
+echo ""
+echo "",
+echo $g"Sukses Download!!!"
+echo ""
+echo $r"File Tersimpan Di sdcard!nama Cepat-Mahir-Python"
+fi
+
+if [ $pil = 3 ] || [ $pil = 3 ]
+then
+clear
+echo "Judul : Belajar-Html&CSS"
+echo "Genre : Pemrograman"
+echo "size  : 1.1 MB"
+sleep 5
+echo $r"Proses."
+sleep 1
+clear
+echo $r"proses.."
+sleep 1
+clear
+echo $r"Proses..."
+sleep 1
+clear
+echo $r"proses...."
+sleep 1
+clear
+echo $r"Proses"
+sleep 1
+clear
+echo $r"proses."
+sleep 1
+clear
+echo $r"Proses.."
+sleep 1
+clear
+echo $r"proses..."
+sleep 1
+clear
+echo $r"Proses...."
+sleep 1
+clear
+echo $r"proses"
+sleep 1
+clear
+echo $r"Proses."
+sleep 1
+clear
+echo $r"proses.."
+sleep 1
+clear
+echo $g"Downloading File"
+sleep 2
+ln -s /sdcard
+cd sdcard
+wget https://endangcahyapermana.files.wordpress.com/2016/03/belajar-html-dan-css.pdf
+echo ""
+echo "",
+echo $g"Sukses Download!!!"
+echo ""
+echo $r"File Tersimpan Di sdcard!nama Belajar-htnl&css"
+fi
+
+if [ $pil = 4 ] || [ $pil = 4 ]
+then
+clear
+echo "Judul : Belajar-Html&PHP"
+echo "Genre : Pemrograman"
+echo "size  : 1.2MB"
+sleep 5
+echo $r"Proses."
+sleep 1
+clear
+echo $r"proses.."
+sleep 1
+clear
+echo $r"Proses..."
+sleep 1
+clear
+echo $r"proses...."
+sleep 1
+clear
+echo $r"Proses"
+sleep 1
+clear
+echo $r"proses."
+sleep 1
+clear
+echo $r"Proses.."
+sleep 1
+clear
+echo $r"proses..."
+sleep 1
+clear
+echo $r"Proses...."
+sleep 1
+clear
+echo $r"proses"
+sleep 1
+clear
+echo $r"Proses."
+sleep 1
+clear
+echo $r"proses.."
+sleep 1
+clear
+echo $g"Downloading File"
+sleep 2
+ln -s /sdcard
+cd sdcard
+wget 
+http://ilmukomputer.org/wp-content/uploads/2012/11/belajar-HTMLPHP-dan-MySQL.pdf
+echo ""
+echo "",
+echo $g"Sukses Download!!!"
+echo ""
+echo $r"File Tersimpan di SD card!Nama belajar-html-php"
+fi
+
+if [ $pil = 5 ] || [ $pil = 5 ]
+then
+clear
+echo "Judul : JavaScript"
+echo "Genre : Pemrograman"
+echo "size  : 1.2MB"
+sleep 5
+echo $r"Proses."
+sleep 1
+clear
+echo $r"proses.."
+sleep 1
+clear
+echo $r"Proses..."
+sleep 1
+clear
+echo $r"proses...."
+sleep 1
+clear
+echo $r"Proses"
+sleep 1
+clear
+echo $r"proses."
+sleep 1
+clear
+echo $r"Proses.."
+sleep 1
+clear
+echo $r"proses..."
+sleep 1
+clear
+echo $r"Proses...."
+sleep 1
+clear
+echo $r"proses"
+sleep 1
+clear
+echo $r"Proses."
+sleep 1
+clear
+echo $r"proses.."
+sleep 1
+clear
+echo $g"Downloading File"
+sleep 2
+ln -s /sdcard
+cd sdcard
+wget http://huda.lecturer.pens.ac.id/Dokumen/WEB/moduljavascript.pdf
+echo ""
+echo "",
+echo $g"Sukses Download!!!"
+echo ""
+echo $r"File Tersimpan di SD card!Nama modul JavaScript"
+fi
+
+if [ $pil = 6 ] || [ $pil = 6 ] 
+then
+clear
+echo $r"Ngapa Exit Begoo."
+sleep 1
+clear
+echo $g"Ngapa Exit Begoo.."
+sleep 1
+clear
+echo $c"Ngapa Exit Begoo..."
+sleep 1
+clear
+echo $r"Ngapa Exit Begoo...."
+sleep 1
+clear
+echo $r"Ngapa Exit Begoo."
+sleel 1
+clear
+echo $g"Ngapa Exit Begoo.."
+sleep 1 
+clear
+echo $c"Ngapa Exit Begoo..."
+sleep 1
+clear
+echo $r"Dadah Goblok !!!"
+exit
+fi

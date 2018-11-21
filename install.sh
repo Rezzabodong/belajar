1464ecd74dc834110bee132d58e838b8a88b1968
R24 <42667691+Rezzabodong@users.noreply.github.com>
2018-11-21 10:09:14 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+b='\033[34;1m'
+g='\033[32;1m'
+p='\033[35;1m'
+c='\033[36;1m'
+r='\033[31;1m'
+w='\033[37;1m'
+y='\033[33;1m'
+echo ""
+echo $r"Sedang menginstall package"
+sleep 0.5
+clear
+echo $r"sEdang menginstall package"
+sleep 0.5
+clear
+echo $r"seDang menginstall package"
+sleep 0.5
+clear
+echo $r"sedAng menginstall package"
+sleep 0.5
+clear
+echo $r"sedaNg menginstall package"
+sleep 0.5
+clear
+echo $r"sedanG menginstall package"
+sleep 0.5
+clear
+echo $r"SEDANG Menginstall package"
+sleep 0.5
+clear
+echo $r"SEDANG mEnginstall package"
+sleep 0.5
+clear
+echo $r"SEDANG meNginstall package"
+sleep 0.5
+clear
+echo $r"SEDANG menGinstall package"
+sleep 0.5
+clear
+echo $r"SEDANG mengInstall package"
+sleep 0.5
+clear
+echo $r"SEDANG mengiNstall package"
+sleep 0.5
+clear
+echo $r"SEDANG menginStall package"
+sleep 0.5
+clear
+echo $r"SEDANG menginTall package"
+sleep 0.5
+clear
+echo $r"SEDANG menginstAll package"
+sleep 0.5
+clear
+echo $r"SEDANG menginStall package"
+sleep 0.5
+clear
+echo $r"SEDANG menginTall package"
+sleep 0.5
+clear
+echo $r"SEDANG menginstAll package"
+sleep 0.5
+clear
+echo $r"SEDANG menginStall package"
+sleep 0.5
+clear
+echo $r"SEDANG menginsTall package"
+sleep 0.5
+clear
+echo $r"SEDANG menginstAll package"
+sleep 0.5
+clear
+echo $r"SEDANG menginstaLl package"
+sleep 0.5
+clear
+echo $r"SEDANG menginstalL package"
+sleep 0.5
+clear
+echo $r"SEDANG MENGINSTALL package"
+sleep 0.5
+clear
+echo $r"SEDANG MENGINSTALL Package"
+sleep 0.5
+clear
+echo $r"SEDANG MENGINSTALL pAckage"
+sleep 0.5
+clear
+echo $r"SEDANG MENGINSTALL paCkage"
+sleep 0.5
+clear
+echo $r"SEDANG MENGINSTALL pacKage"
+sleep 0.5
+clear
+echo $r"SEDANG MENGINSTALL packAge"
+sleep 0.5
+clear
+echo $r"SEDANG MENGINSTALL packaGe"
+sleep 0.5
+clear
+echo $r"SEDANG MENGINSTALL packagE"
+sleep 0.5
+clear
+echo $r"SEDANG MENGINSTALL PACKAGE"
+sleep 0.5
+mkdir module
+mv -f belajar.sh /$HOME/belajar/module
+apt-get install nodejs
+npm install figlet
+npm install -g figlet cli
+pkg install ruby
+gem install lolcat
+echo ""
+echo ""
+echo ""
+echo ""
+echo $g" S U K S E S - T E R I N S T A L L - B O S "
+sleep 1
+echo ""
+echo ""
+echo $g" U N T U K - M E N J A L A N K A N - S C R I P T"
+sleep 1
+echo ""
+echo ""
+echo $g" K E T I K :  sh start.sh "
+sleep 5
+echo ""
+echo ""
+echo ""

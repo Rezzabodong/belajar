80ac48b5bcac5407bb0ffeaf02e4841dddb26260
R24 <42667691+Rezzabodong@users.noreply.github.com>
2018-11-21 10:02:44 +0000

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,54 @@
+!/bin/bash
+b='\033[34;1m'
+g='\033[32;1m'
+p='\033[35;1m'
+c='\033[36;1m'
+r='\033[31;1m'
+w='\033[37;1m'
+y='\033[33;1m'
+echo $r"Connecting"
+sleep 1
+clear
+echo $g"Connecting"
+sleep 1
+clear
+echo $c"Connecting"
+sleeo 1
+clear
+echo $g"Connecting"
+sleep 1
+clear
+echo $y"Connecting"
+sleep 1
+clear
+echo $r"waiting For Respones."
+sleep 1
+clear
+echo $r"Waiting For Respones.."
+sleep 1
+clear
+echo $r"Waiting For Respones..."
+sleep 1
+clear
+echo $r"Waiting For Respones"
+sleep 1
+clear
+echo $r"Waiting For Respones."
+sleep 1
+clear
+echo $r"Waiting For Respones.."
+sleep 1
+clear
+echo $r"Waiting For Respones..."
+sleep 1
+clear
+echo $r"Waiting For Respones"
+sleep 1
+clear
+echo $r"http://Requsest 200 OK"
+sleep 2
+clear
+echo $g"Connected"
+sleep 3
+cd module
+sh belajar.sh
